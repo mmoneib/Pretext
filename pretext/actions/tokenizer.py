cffb31e2794fdd4fe1ae0dfd374b43dfc0ebe8f2
Muhammad Moneib <mmoneib@gmx.com>
2023-11-12 02:26:23 +0100

    Added tokenization by multiple chars and words; triggered Fibonacci tokenization.

--- pretext/actions/tokenizer.py
+++ pretext/actions/tokenizer.py
@@ -4,14 +4,50 @@ def tokenize_by_char(text):
     tokens+=c
   return tokens
 
+def tokenize_by_chars(text, numOfChars): # Another function as one char doesn't need a buffer.
+  tokens=[]
+  buff=""
+  count=0
+  for c in text:
+    buff+=c
+    if count == numOfChars-1:
+      tokens.append(buff)
+      buff=""
+      count=8
+    count=count+1
+  return tokens
+
 def tokenize_by_word(text):
   tokens=[]
   word=""
   for c in text:
     if c == ' ' or c == '\n':
       if word != "":
-        tokens.append(word) # Must use append; otherwise, it would be a list of chars.
+        tokens.append(word) # Must use append; otherwise, it would be a list of chars. Adding c for organic prediction of spaces and conclusions.
       word=""
+    else:
+      word+=c
+  return tokens
+
+def tokenize_by_words(text, numOfWords): # Another function as one char doesn't need a buffer.
+  tokens=[]
+  word=""
+  buff=""
+  count=0
+  for c in text:
+    if c == ' ' or c == '\n': #TODO Allow space and new line before a word but not after.
+      if word.endswith(' ') == False and word.endswith('\n') == False: # That it is not a string of empty spaces.
+        buff+=word # Can include empty spaces prior to the work.
+        if count == numOfWords-1:
+          tokens.append(buff)
+          buff=""
+          word=""
+          count=0
+        else:
+          word+=c
+        count=count+1
+      #else:
+        #word+=c # Adding empty space to empty spaces.
     else:
       word+=c
   return tokens
--- pretext/pretext.py
+++ pretext/pretext.py
@@ -8,12 +8,14 @@ from process import writer
 from model.configuration import Configuration
 from model.knowledge_graph import KnowledgeGraph
 
-#TODO Allow differing grains of tokenization: 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 chars, words, and keywords.
+#TODO Check if output off multiple words tokens is correct.
+#TODO Check feasibility of tokenization by keywords.
 #TODO Persist the models.
 #TODO Make process modules int classes of input, process, and output workflow.
 #TODO Add logging.
 #TODO Add header comments to functions.
 #TODO Distinguish between word and part of word, like to and token.
+#TODO Add unit tests.
 
 if __name__=="__main__":
   parser = argparse.ArgumentParser(description='Pretext predicts text based on input document(s) (scope of knowledge), statistical formula (method of analysis), and a trigger text for the output along with its size (destiny).')
@@ -25,7 +27,19 @@ if __name__=="__main__":
   knowledgeGraph=KnowledgeGraph()
   for output in reader.read(args.knowledge_files):
     tokens=tokenizer.tokenize_by_char(output)
+    tokens.extend(tokenizer.tokenize_by_chars(output,2))
+    tokens.extend(tokenizer.tokenize_by_chars(output,3))
+    tokens.extend(tokenizer.tokenize_by_chars(output,5))
+    tokens.extend(tokenizer.tokenize_by_chars(output,8))
+    tokens.extend(tokenizer.tokenize_by_chars(output,13))
+    tokens.extend(tokenizer.tokenize_by_chars(output,21))
     tokens.extend(tokenizer.tokenize_by_word(output)) # Merges the results of tokenizer with the previous results.
+    tokens.extend(tokenizer.tokenize_by_words(output,2))
+    tokens.extend(tokenizer.tokenize_by_words(output,3))
+    tokens.extend(tokenizer.tokenize_by_words(output,5))
+    tokens.extend(tokenizer.tokenize_by_words(output,8))
+    tokens.extend(tokenizer.tokenize_by_words(output,13))
+    tokens.extend(tokenizer.tokenize_by_words(output,21))
     knowledgeGraph=modeler.model_by_next(tokens, knowledgeGraph)
   print()
   print(statistician.analyze(knowledgeGraph).get_report())
