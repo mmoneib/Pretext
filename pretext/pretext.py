14b28cc4940caea188233d93fd789d7ce7d9c03f
Muhammad Moneib <mmoneib@gmx.com>
2023-11-17 08:45:04 +0100

    Add tokenization lists parameters.

--- pretext/pretext.py
+++ pretext/pretext.py
@@ -16,12 +16,14 @@ from model.knowledge_graph import KnowledgeGraph
 #TODO Add more unit tests for effect of empty spaces on tokenization.
 #TODO ALlow assymmetrical modeling, like 3 unconsecutive chars predicting a word (instead of another 2 unconsecutive chars).
 #TODO Parse input directories.
+#TODO Use tokenization steps as an input parameter.
 
 if __name__=="__main__":
   parser = argparse.ArgumentParser(description="Pretext predicts text based on input document(s) (scope of knowledge), statistical formula (method of analysis), and a trigger text for the output along with its size (destiny).")
-  parser.add_argument("-k", "--knowledge-files", nargs='*',
-                    help="path to files to be added to the scope of knowledge.", required=True)
+  parser.add_argument("-c", "--chars-tokenization-steps", help="list of integers specifying the number of characters considered in each tokenization step", nargs='*')
+  parser.add_argument("-k", "--knowledge-files", help="path to files to be added to the scope of knowledge.", nargs='*', required=True)
   parser.add_argument("-n", "--infinite-prompting", help="cycle between prompts and subsequent predictions until |exit| is typed.", action="store_true")
+  parser.add_argument("-w", "--words-tokenization-steps", help="list of integers specifying the number of words considered in each tokenization step", nargs='*')
   args = parser.parse_args()
   config=Configuration()
   config.infinitePrompting=args.infinite_prompting
