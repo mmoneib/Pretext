826c9594fba6986b78669e29456741ff627ff573
Muhammad Moneib <mmoneib@gmx.com>
2024-12-14 23:06:25 +0100

    Added resources and more unit tests for test_file_actions.

--- tests/test_file_actions.py
+++ tests/test_file_actions.py
@@ -4,8 +4,22 @@ import pretext.actions.file as FileActions
 
 class TestFileActions(unittest.TestCase):
 
-  def test_read_text_file(self):
-    data = FileActions.read_text_file("tests/resources/test.txt")
+  def test_read_text_file_single_line(self):
+    data = FileActions.read_text_file("tests/resources/test_single_line.txt")
     # Reading a file appends a new-line to the end of the read text.
     self.assertEqual(data, "qwertyuiop\n")
 
+  def test_read_text_file_multi_line(self):
+    data = FileActions.read_text_file("tests/resources/test_multi_line.txt")
+    # Reading a file appends a new-line to the end of the read text.
+    self.assertEqual(data, "asdfghjkl\nzxcvbnm\n")
+
+  def test_read_text_file_end_with_newline(self):
+    data = FileActions.read_text_file("tests/resources/test_end_with_newline.txt")
+    # Reading a file appends a new-line to the end of the read text.
+    self.assertEqual(data, "A\n\n")
+
+  def test_read_text_file_symbols(self): # Mainly to test escaping and supportability.
+    data = FileActions.read_text_file("tests/resources/test_symbols.txt")
+    # Reading a file appends a new-line to the end of the read text. Double quotation and backspace are escaped by a preceding backspace.
+    self.assertEqual(data, "`~!@#$%^&*()_+=-\\';:\"|/.,<>?\n")
